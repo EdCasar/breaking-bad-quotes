cc0d02cab979f1e2aff45767fc3b5ec26e2c6524
Edcas <edcasdevelopment@gmail.com>
2021-05-22 01:33:52 -0500

    se crea estado uniendo cada character y sus repectivas quotes

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,25 +1,45 @@
+import {useState, useEffect} from 'react';
+
 import useQuotes from '../hooks/useQuotes';
 import useCharacters from '../hooks/useCharacters';
 
 const Home = () => {
-  const quote = useQuotes();
-  const character = useCharacters();
+	const quote = useQuotes();
+	const character = useCharacters();
+	const [charAndQuote, setCharAndQuote] = useState([]);
 
-  return (
-    <div>
-      <h1>Home</h1>
-      {character ? (
-        character.map(({char_id, name, img}) => (
-          <div key={char_id}>
-            <img src={img} alt="" width="200" />
-            <p>{name}</p>
-          </div>
-        ))
-      ) : (
-        <p>No found </p>
-      )}
-    </div>
-  );
+	useEffect(() => {
+		if (character) {
+			character.map(each => {
+				if (quote) {
+					const nameAll = quote.filter(fil => fil.author === each.name);
+					if (nameAll.length > 0) {
+						each.quotes = nameAll;
+						setCharAndQuote(state => [...state, each]);
+					}
+				}
+			});
+		}
+	}, [character, quote]);
+	return (
+		<div>
+		<h1>Home</h1>
+		{charAndQuote ? (
+			charAndQuote.map(({char_id, name, img, quote, quote_id, quotes}) => (
+				<div key={Math.random()}>
+				<img src={img} width="200" alt="" />
+				<p>{name}</p>
+
+				{quotes
+					? quotes.map(ea => <li key={ea.quote_id}>{ea.quote}</li>)
+					: null}
+				</div>
+			))
+		) : (
+			<p>Loading...</p>
+		)}
+		</div>
+	);
 };
 
 export default Home;
